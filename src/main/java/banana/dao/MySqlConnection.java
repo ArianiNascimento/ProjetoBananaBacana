555feb71f1ce1f21e0d72417770848ab64b0d74e
ArianiNascimento <arianiferreira4@gmail.com>
2022-04-27 15:00:04 -0300

    primeiro commit

---
+++ src/main/java/banana/dao/MySqlConnection.java
@@ -0,0 +1,12 @@
+package banana.dao;
+
+import java.sql.Connection;
+
+public class MySqlConnection {
+	
+	public Connection getConnection() {
+		
+		Connection conn = null;
+		return conn;
+	}
+}
---
+++ src/main/java/banana/dao/ProdutoDao.java
@@ -0,0 +1,5 @@
+package banana.dao;
+
+public class ProdutoDao {
+	
+}
